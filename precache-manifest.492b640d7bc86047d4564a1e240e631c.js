bcb3a1ea67395e3924b7af37c8e1f5cbe0b5b886
Sergey X <serhento@gmail.com>
2020-05-08 00:45:34 +0300

    Updates

--- precache-manifest.492b640d7bc86047d4564a1e240e631c.js
+++ precache-manifest.492b640d7bc86047d4564a1e240e631c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b3adc23aaac10c603c4005d1e3d05ad2",
+    "revision": "600beeac6a1ce73caef3268ed5d77816",
     "url": "/Note-Calc/index.html"
   },
   {
-    "revision": "7145e5243173794bca40",
+    "revision": "448b0aa1a551a716626d",
     "url": "/Note-Calc/static/css/main.ea01e7f3.chunk.css"
   },
   {
-    "revision": "65124404418c70f3275a",
-    "url": "/Note-Calc/static/js/2.2db78177.chunk.js"
+    "revision": "268b261a35cd86337d08",
+    "url": "/Note-Calc/static/js/2.8aba009f.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/Note-Calc/static/js/2.2db78177.chunk.js.LICENSE.txt"
+    "url": "/Note-Calc/static/js/2.8aba009f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7145e5243173794bca40",
-    "url": "/Note-Calc/static/js/main.89668911.chunk.js"
+    "revision": "448b0aa1a551a716626d",
+    "url": "/Note-Calc/static/js/main.33b7e4b5.chunk.js"
   },
   {
     "revision": "938f6f5076e6f275c29f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Note-Calc/precache-manifest.1ba7cca8c1b00137b63f2387b788e122.js"
+  "/Note-Calc/precache-manifest.492b640d7bc86047d4564a1e240e631c.js"
 );
 
 self.addEventListener('message', (event) => {
