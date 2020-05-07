21e92bf49df454341dd57494b6164d2916aa751a
Sergey X <serhento@gmail.com>
2020-05-07 21:31:54 +0300

    Updates

---
+++ precache-manifest.9d211cd5199d83c8359c2a7d82ab169f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c125a4a7aefea2c23529fa94e2f75b",
+    "url": "/Note-Calc/index.html"
+  },
+  {
+    "revision": "61266ad2492ad15eda17",
+    "url": "/Note-Calc/static/css/main.ea01e7f3.chunk.css"
+  },
+  {
+    "revision": "65124404418c70f3275a",
+    "url": "/Note-Calc/static/js/2.2db78177.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Note-Calc/static/js/2.2db78177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61266ad2492ad15eda17",
+    "url": "/Note-Calc/static/js/main.7917a89a.chunk.js"
+  },
+  {
+    "revision": "938f6f5076e6f275c29f",
+    "url": "/Note-Calc/static/js/runtime-main.11440b5d.js"
+  }
+]);
